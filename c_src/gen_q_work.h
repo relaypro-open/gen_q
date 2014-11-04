849c83e144d0ce9167ab4666477891ab27c17dbf
Jesse Stimpson <jstimpson@bandwidth.com>
2014-11-04 12:02:36 -0500

    fix extern

--- c_src/gen_q_work.h
+++ c_src/gen_q_work.h
@@ -8,8 +8,8 @@ typedef struct {
     long timeout;
 } QWork;
 
-void genq_work(void *data);
-void* genq_malloc_work(char *buff, ErlDrvSizeT bufflen);
-void genq_free_work(void *work);
+extern void genq_work(void *data);
+extern void* genq_malloc_work(char *buff, ErlDrvSizeT bufflen);
+extern void genq_free_work(void *work);
 
 #endif
