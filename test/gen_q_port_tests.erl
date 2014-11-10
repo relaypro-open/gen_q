ca85cab71388de9c64f2d829bd0100e93ad7a5c1
Jesse Stimpson <jstimpson@bandwidth.com>
2014-11-10 09:48:32 -0500

    a real test

--- test/gen_q_port_tests.erl
+++ test/gen_q_port_tests.erl
@@ -1,10 +1,13 @@
 -module(gen_q_port_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(EQE(Type, Value), ?assertMatch({ok, {Type, Value}}, gen_q_port:apply(P, H, "(::)", Type, Value))).
+
 e2q2e_test() ->
     {ok, Q} = test_q:start(500),
     {ok, P} = gen_q_port:start([]),
     {ok, H} = gen_q_port:hopen(P, "localhost", 5000, "us:pa", 1000),
+    ?EQE(integer, 2),
     test_q:stop(Q, P, H).
     % For some reason, stoppping the port causes rebar to segfault
     %ok = gen_q_port:stop(P),
