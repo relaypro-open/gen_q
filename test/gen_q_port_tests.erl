1388c2d64581a590f32d3e26f6a0fda959597aa0
Jesse Stimpson <jstimpson@bandwidth.com>
2014-11-10 20:41:20 -0500

    cleanup names, add neightboor assertions

--- test/gen_q_port_tests.erl
+++ test/gen_q_port_tests.erl
@@ -2,33 +2,8 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("gen_q_test.hrl").
 
--define(_EQE(Type, Value), ?_assertMatch({ok, {Type, Value}}, gen_q_port:apply(P, H, "(::)", Type, Value))).
--define(_EQE2(Type, InValue, OutValue), ?_assertMatch({ok, {Type, OutValue}}, gen_q_port:apply(P, H, "(::)", Type, InValue))).
--define(_EQE3(InType, OutType, Value), ?_assertMatch({ok, {OutType, Value}}, gen_q_port:apply(P, H, "(::)", InType, Value))).
-
--define(EQE_NEIGHBOR(Type, Value, Eps),
-          begin
-                  ((fun (__ExpectType, __ExpectValue) ->
-                      case gen_q_port:apply(P, H, "(::)", Type, Value) of
-                          {ok, {__ExpectType, __Value2}} ->
-                              ?assertNeighborhood(__ExpectValue, __Value2, Eps);
-                          {ok, {__Type2, _}} ->
-                              erlang:error({'EQE_NEIGHBOOR_type_failed',
-                                      [{module, ?MODULE},
-                                          {line, ?LINE},
-                                          {expression, {(??Type), (??Value)}},
-                                          {expected, __ExpectType},
-                                          {value, __Type2}]});
-                          __OtherResult ->
-                              erlang:error({'EQE_NEIGHBOOR_failed',
-                                      [{module, ?MODULE},
-                                          {line, ?LINE},
-                                          {expression, {(??Type), (??Value)}},
-                                          {result, __OtherResult}]})
-                      end
-              end)(Type, Value))
-    end).
--define(_EQE_NEIGHBOOR(Type, Value, Eps), ?_test(?EQE_NEIGHBOR(Type, Value, Eps))).
+-define(realEps, 0.0001).
+-define(floatEps, 0.000001).
 
 -record(ctx, {qpid, port, h}).
 
@@ -37,43 +12,43 @@ gen_q_port_test_() ->
             fun setup/0,
             fun teardown/1,
             fun(#ctx{port=P, h=H}) ->
-                    [?_EQE(time, 43200000), % `int$12t
-                     ?_EQE(second, 45042), % `int$`second$12:30:42
-                     ?_EQE(minute, 750), % `int$`minute$12:30:42
-                     ?_EQE(timespan, 19647590000), % `long$2014.11.10D16:27:40.805091000-2014.11.10D16:27:21.157501000
-                     ?_EQE(datetime, 5427.693), % `float$2014.11.10T16:38:04.679
-                     ?_EQE(date, 5427), % `int$2014.11.10
-                     ?_EQE(month, 178), % `int$2014.11m
-                     ?_EQE(integer, 2),
-                     ?_EQE(timestamp, 468953337121125000), % `long$2014.11.10D16:48:57.121125000
-                     ?_EQE(long, 2),
-                     ?_EQE(float, 0.456),
-                     ?_EQE(char, $A),
-                     ?_EQE_NEIGHBOOR(real, 0.1, 0.0001),
-                     ?_EQE(short, 3),
-                     ?_EQE(byte, 255),
-                     ?_EQE(boolean, 1),
-                     ?_EQE(symbol, 'hello'),
-                     ?_EQE(string, "hello"),
-                     ?_EQE2(string, <<"hello">>, "hello"),
-                     ?_EQE({list, symbol}, [abc, def]),
-                     ?_EQE({list, integer}, [1,2,3]),
-                     ?_EQE({list, long}, [10,11,12]),
-                     ?_EQE({list, byte}, [5,6,7]),
-                     ?_EQE({list, short}, [15,16,17]),
-                     ?_EQE({list, real}, [1.1, 2.2, 3.3]),
-                     ?_EQE({list, float}, [5.5, 6.5, 7.5])
+                    [?_eqe(time, 43200000), % `int$12t
+                     ?_eqe(second, 45042), % `int$`second$12:30:42
+                     ?_eqe(minute, 750), % `int$`minute$12:30:42
+                     ?_eqe(timespan, 19647590000), % `long$2014.11.10D16:27:40.805091000-2014.11.10D16:27:21.157501000
+                     ?_eqe_neighbor(datetime, 5427.693, ?floatEps), % `float$2014.11.10T16:38:04.679
+                     ?_eqe(date, 5427), % `int$2014.11.10
+                     ?_eqe(month, 178), % `int$2014.11m
+                     ?_eqe(integer, 2),
+                     ?_eqe(timestamp, 468953337121125000), % `long$2014.11.10D16:48:57.121125000
+                     ?_eqe(long, 2),
+                     ?_eqe_neighbor(float, 0.456, ?floatEps),
+                     ?_eqe(char, $A),
+                     ?_eqe_neighbor(real, 0.1, ?realEps),
+                     ?_eqe(short, 3),
+                     ?_eqe(byte, 255),
+                     ?_eqe(boolean, 1),
+                     ?_eqe(symbol, 'hello'),
+                     ?_eqe(string, "hello"),
+                     ?_eqe_value(string, <<"hello">>, "hello"),
+                     ?_eqe({list, symbol}, [abc, def]),
+                     ?_eqe({list, integer}, [1,2,3]),
+                     ?_eqe({list, long}, [10,11,12]),
+                     ?_eqe({list, byte}, [5,6,7]),
+                     ?_eqe({list, short}, [15,16,17]),
+                     ?_eqe_all_neighbor({list, real}, [1.1, 2.2, 3.3], ?realEps),
+                     ?_eqe_all_neighbor({list, real}, [5.5, 6.5, 7.5], ?floatEps)
                  ] ++
 
                  % Test erlang encoding lists of small ints as strings:
-                 [ ?_EQE({list, X}, [1,2,3]) || X <-
+                 [ ?_eqe({list, X}, [1,2,3]) || X <-
                      [time, second, minute, date, month,
                          timespan, timestamp, byte, boolean,
                          short, datetime, float, real, integer, long] ] ++
-                 [ ?_EQE3({list, char}, string, [1,2,3])] ++
+                 [ ?_eqe_type({list, char}, string, [1,2,3])] ++
 
                  % Test erlang encoding lists of big ints as lists:
-                 [ ?_EQE({list, X}, [1000,2000,3000]) || X <-
+                 [ ?_eqe({list, X}, [1000,2000,3000]) || X <-
                      [time, second, minute, date, month,
                          timespan, timestamp, byte, boolean,
                          short, datetime, float, real, integer, long] ]
@@ -84,9 +59,9 @@ gen_q_port_utiqd_test_() ->
             fun setup_utiqd/0,
             fun teardown/1,
             fun(#ctx{port=P, h=H}) ->
-                    [?_EQE(datetime, 1415630000),
-                     ?_EQE({list, datetime}, [1,2,3]),
-                     ?_EQE({list, datetime}, [1000,2000,3000])]
+                    [?_eqe(datetime, 1415630000),
+                     ?_eqe({list, datetime}, [1,2,3]),
+                     ?_eqe({list, datetime}, [1000,2000,3000])]
             end}.
 
 gen_q_port_dsiqt_test_() ->
@@ -94,9 +69,9 @@ gen_q_port_dsiqt_test_() ->
             fun setup_dsiqt/0,
             fun teardown/1,
             fun(#ctx{port=P, h=H}) ->
-                    [?_EQE(time, 43200),
-                     ?_EQE({list, time}, [1,2,3]),
-                     ?_EQE({list, time}, [1000,2000,3000])]
+                    [?_eqe(time, 43200),
+                     ?_eqe({list, time}, [1,2,3]),
+                     ?_eqe({list, time}, [1000,2000,3000])]
             end}.
 
 setup() ->
--- test/gen_q_test.hrl
+++ test/gen_q_test.hrl
@@ -14,3 +14,74 @@
                     end)(Expect))
     end).
 -define(_assertNeighborhood(Expect, Expr, Epsilon), ?_test(?assertNeighborhood(Expect, Expr, Epsilon))).
+
+-define(assertAllNeighborhood(Expect, Expr, Epsilon),
+    begin
+        ((fun (__X) ->
+            __V = (Expr),
+            case lists:all(fun({__X1, __V1}) ->
+                            abs(__V1-__X1) < Epsilon
+                    end, lists:zip(__X, __V)) of
+                true -> ok;
+                false -> erlang:error({assertAllNeighborhood_failed,
+                                            [{module, ?MODULE},
+                                                {line, ?LINE},
+                                                {expression, (??Expr)},
+                                                {expected, {Expect, '+/-', Epsilon}},
+                                                {value, __V}]})
+                            end
+                    end)(Expect))
+    end).
+-define(_assertAllNeighborhood(Expect, Expr, Epsilon), ?_test(?assertAllNeighborhood(Expect, Expr, Epsilon))).
+
+-define(_eqe(Type, Value), ?_assertMatch({ok, {Type, Value}}, gen_q_port:apply(P, H, "(::)", Type, Value))).
+-define(_eqe_value(Type, InValue, OutValue), ?_assertMatch({ok, {Type, OutValue}}, gen_q_port:apply(P, H, "(::)", Type, InValue))).
+-define(_eqe_type(InType, OutType, Value), ?_assertMatch({ok, {OutType, Value}}, gen_q_port:apply(P, H, "(::)", InType, Value))).
+
+-define(eqe_neighbor(Type, Value, Eps),
+          begin
+                  ((fun (__ExpectType, __ExpectValue) ->
+                      case gen_q_port:apply(P, H, "(::)", Type, Value) of
+                          {ok, {__ExpectType, __Value2}} ->
+                              ?assertNeighborhood(__ExpectValue, __Value2, Eps);
+                          {ok, {__Type2, _}} ->
+                              erlang:error({'eqe_neighbor_type_failed',
+                                      [{module, ?MODULE},
+                                          {line, ?LINE},
+                                          {expression, {(??Type), (??Value)}},
+                                          {expected, __ExpectType},
+                                          {value, __Type2}]});
+                          __OtherResult ->
+                              erlang:error({'eqe_neighbor_failed',
+                                      [{module, ?MODULE},
+                                          {line, ?LINE},
+                                          {expression, {(??Type), (??Value)}},
+                                          {result, __OtherResult}]})
+                      end
+              end)(Type, Value))
+    end).
+-define(_eqe_neighbor(Type, Value, Eps), ?_test(?eqe_neighbor(Type, Value, Eps))).
+
+-define(eqe_all_neighbor(Type, Value, Eps),
+    begin
+            ((fun (__ExpectType, __ExpectValue) ->
+                case gen_q_port:apply(P, H, "(::)", Type, Value) of
+                    {ok, {__ExpectType, __Value2}} ->
+                        ?assertAllNeighborhood(__ExpectValue, __Value2, Eps);
+                    {ok, {__Type2, _}} ->
+                        erlang:error({'eqe_all_neighboor_failed',
+                                [{module, ?MODULE},
+                                 {line, ?LINE},
+                                 {expression, {(??Type), (??Value)}},
+                                 {expected, __ExpectType},
+                                 {value, __Type2}]});
+                    __OtherResult ->
+                        erlang:error({'eqe_all_neighbor_failed',
+                                [{module, ?MODULE},
+                                    {line, ?LINE},
+                                    {expression, {(??Type), (??Value)}},
+                                    {result, __OtherResult}]})
+                end
+        end)(Type, Value))
+    end).
+-define(_eqe_all_neighbor(Type, Value, Eps), ?_test(?eqe_all_neighbor(Type, Value, Eps))).
