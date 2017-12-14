0a01ed0f62c8841cfe121bafb28d0900498c72ef
Jesse Stimpson <jesse.stimpson@gmail.com>
2017-12-14 10:36:59 -0500

    reql fixes and base64

--- c_src/q.c
+++ c_src/q.c
@@ -54,6 +54,7 @@ int sprintf_j(char *buf, long long j);
 int sprintf_f(char *buf, double d);
 int sprintf_z(char *buf, double d);
 int sprintf_p(char *buf, long long j);
+unsigned char * genq_base64_encode(const unsigned char *src, size_t len, size_t *out_len);
 
 K global_sym = 0;
 
@@ -507,17 +508,22 @@ int ei_x_q_dbnext(QWorkDbOp* data, long num_records, QOpts* opts) {
     short short_ = 0;
     char buffer[1024] = {0};
     int ok = 0;
+    size_t size_t_ = 0;
 
     int private_data_column_pos = -1;
 
 #ifndef DBOP_EI
     int jind = 0;
+    unsigned char* b64 = 0;
 #endif
 
     EI(ei_x_new(&data->x));
     data->has_x = 1;
 
-    jind += sprintf(json + jind, "[");
+#ifndef DBOP_EI
+    // Specify [2,[]] for rethink-compatible json array
+    jind += sprintf(json + jind, "[2,[");
+#endif
     int i=0;
     for(i=0; i < num_records; ++i) {
 
@@ -927,17 +933,27 @@ int ei_x_q_dbnext(QWorkDbOp* data, long num_records, QOpts* opts) {
                 case 87: // special string type
                     fptr = (FILE*)kJ(data_handle_column)[j];
 
-                    char* string_ = genq_alloc(1+(sizeof(char))*(long_-pos));
+                    unsigned char* string_ = genq_alloc(1+(sizeof(unsigned char))*(long_-pos));
                     ok = (long_-pos) == fread(string_, 1, long_-pos, fptr);
                     string_[long_-pos] = 0;
                     if(!ok) {
                         jind += sprintf(json + jind, "null");
                         ok = 1;
+                    } else if (is_reql_bin) {
+                        if(long_-pos > 0) {
+                            LOG("dbnext reql bin size %lld\n", long_-pos);
+                            b64 = genq_base64_encode(string_, (size_t)long_-pos, &size_t_);
+                            if(b64) {
+                                jind += sprintf(json + jind, "\"%s\"}", b64);
+                            } else {
+                                jind += sprintf(json + jind, "\"\"}");
+                            }
+                            genq_free(b64);
+                        } else {
+                            jind += sprintf(json + jind, "\"\"}");
+                        }
                     } else {
                         jind += sprintf(json + jind, "\"%s\"", string_);
-                    }
-                    if(is_reql_bin) {
-                        jind += sprintf(json + jind, "}");
                     }
                     genq_free(string_);
 
@@ -971,7 +987,7 @@ int ei_x_q_dbnext(QWorkDbOp* data, long num_records, QOpts* opts) {
     }
 #else
     if(return_data) {
-        jind += sprintf(json + jind, "]");
+        jind += sprintf(json + jind, "]]");
 
         EI(ei_x_encode_binary(&data->x, json, jind));
     } else {
@@ -1324,4 +1340,78 @@ void q_set_le(int ktype, G* x, int y) {
         x[2] = (y >> 16) & 0xFF;
         x[3] = (y >> 24) & 0xFF;
     }
+}
+/*
+ * Base64 encoding/decoding (RFC1341)
+ * Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
+
+static const unsigned char base64_table[65] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+/**
+ * base64_encode - Base64 encode
+ * @src: Data to be encoded
+ * @len: Length of the data to be encoded
+ * @out_len: Pointer to output length variable, or %NULL if not used
+ * Returns: Allocated buffer of out_len bytes of encoded data,
+ * or %NULL on failure
+ *
+ * Caller is responsible for freeing the returned buffer. Returned buffer is
+ * nul terminated to make it easier to use as a C string. The nul terminator is
+ * not included in out_len.
+ */
+unsigned char * genq_base64_encode(const unsigned char *src, size_t len,
+			      size_t *out_len) {
+    LOG("base64_encode len=%zu\n", len);
+	unsigned char *out, *pos;
+	const unsigned char *end, *in;
+	size_t olen;
+
+	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+	olen += olen / 72; /* line feeds */
+	olen++; /* nul termination */
+
+    LOG("base64_encode olen=%zu\n", olen);
+
+	if (olen < len)
+		return NULL; /* integer overflow */
+	out = genq_alloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	end = src + len;
+	in = src;
+	pos = out;
+	while (end - in >= 3) {
+        LOG("base64_encode loop %ld\n", end-in);
+		*pos++ = base64_table[in[0] >> 2];
+		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+		*pos++ = base64_table[in[2] & 0x3f];
+		in += 3;
+	}
+
+	if (end - in) {
+        LOG("base64_encode final %ld\n", end-in);
+		*pos++ = base64_table[in[0] >> 2];
+		if (end - in == 1) {
+			*pos++ = base64_table[(in[0] & 0x03) << 4];
+			*pos++ = '=';
+		} else {
+			*pos++ = base64_table[((in[0] & 0x03) << 4) |
+					      (in[1] >> 4)];
+			*pos++ = base64_table[(in[1] & 0x0f) << 2];
+		}
+		*pos++ = '=';
+	}
+
+	*pos = '\0';
+    LOG("base64_encode returning %d\n", 0);
+	if (out_len)
+		*out_len = pos - out;
+	return out;
 }
